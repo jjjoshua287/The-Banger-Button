a300885046f54f074ab5c049b819421f76aa6e2a
jjjoshua287 <jjjoshua287@gmail.com>
2025-10-05 18:18:22 -0400

    1st Commit
    1st commit -> Uploaded HTML, CSS, and JavaScript Files

---
+++ script.js
@@ -0,0 +1,25 @@
+///Constructor Function for Screen Transitions
+    function screenTransition(screen1,screen2,displayType){
+      currentPage = document.querySelector(screen1);
+      desiredPage = document.querySelector(screen2);
+      currentPage.style.display = "none"
+      desiredPage.style.display = displayType;
+        }
+
+    function startGame(){
+        screenTransition(".present", ".buttonDiv", "flex");
+        let count = 0
+        const theButton = document.getElementById("theButton");
+        theButton.addEventListener("click", function(){
+            count++;
+            theButton.innerHTML = count;
+            if(checkForVictory(count)){
+                screenTransition(".buttonDiv", ".end", "flex");
+                return
+            }
+        });
+    }
+    function checkForVictory(counter){
+        victoryCondition = 1000 //Number of clicks to win da game
+        return counter >= victoryCondition
+    }
